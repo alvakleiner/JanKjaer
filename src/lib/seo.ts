66abb46ea226b2b32d0719d90a846bdf70ce9827
alvakleiner <alvakleiner@gmail.com>
2026-03-07 23:53:59 +0100

    content: change seo description

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -1,6 +1,6 @@
 export const SeoInfo = {
   title: "Jan Kjærstad - Storyteller",
-  description: "Den offisielle nettsiden til Jan Kjærstad - prisvinnende norsk forfatter.",
+  description: "Den offisielle nettsiden til Jan Kjærstad - en av Norges fremste romanforfattere, kjent for Jonas Wergeland-trilogien og Nordisk råds litteraturpris 2001.",
   url: "https://jankjaerstad.no",
   bannerImage: "https://jankjaerstad.no/images/pappa.jpg",
   name: "Jan Kjærstad",
